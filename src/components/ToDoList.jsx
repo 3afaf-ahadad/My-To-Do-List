a66e18d6c976aeaa1351181fa1f5fa34f5d57939
Afaf <afaf20ahadad@gmail.com>
2025-11-28 21:11:08 +0100

    The structure of the Application

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,6 +1,36 @@
 import { useState } from "react";
 
 function ToDoList() {
-  return <></>;
+  const [tasks, setTasks] = useState([
+    "👩🏽‍💻Coding",
+    "🎮 Playing games",
+    " 🍜Eating",
+  ]);
+
+  function addTask(e) {}
+  function deleteTask() {}
+  function moveTaskUp() {}
+  function moveTaskDown() {}
+
+  return (
+    <>
+      <div className="todolist-container">
+        <h1>To Do List: </h1>
+        <div className="add-tasks">
+          <input type="text" placeholder="Enter your task... " />
+          <button>Add</button>
+        </div>
+        <div className="tasks-container">
+          {tasks.map((t, index) => (
+            <li key={index}>
+              {" "}
+              <button>Up</button> <button>Down</button> {t}{" "}
+              <button>delete</button>
+            </li>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 }
 export default ToDoList;
